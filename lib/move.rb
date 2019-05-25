4d72c6d6dd03e69595ddcdfd51ad5646ae1f46fc
Farzan Khoobchehr <farzan_khoobchehr@yahoo.com>
2019-05-25 22:29:41 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,8 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(input)
-  index = input.to_i - 1
+def input_to_index(varfar)
+  index = varfar.to_i - 1
 end
 
 def move(board, index, token="X")
